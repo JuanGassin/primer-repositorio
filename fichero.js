2d1457af87f63c0288d396ea46fee9cd1d064177
SrGassin <juanjosefloresgassin@fp.iesromerovargas.com>
2022-04-06 02:41:17 +0200

    Confirmar

--- fichero.js
+++ fichero.js
@@ -4,5 +4,6 @@ function Suma(){
     a = parseInt(document.getElementById("a").value);
     b = parseInt(document.getElementById("b").value);
     
-    window.alert(a + b);
+    window.alert(a + b); 
+    window.alert("Gracias por confiar en el programa de JS de Juan José Flores Gassín")
 }
